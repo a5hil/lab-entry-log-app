87ec8a605a7ad3ec3dec4b1932e39f5dcd86b819
a5hil <ashil555anto@gmail.com>
2026-07-14 15:16:52 +0530

    Form fix done

--- src/Components/AddLabEntry.jsx
+++ src/Components/AddLabEntry.jsx
@@ -47,19 +47,42 @@ const AddLabEntry = () => {
                         <div className="row g-4">
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="form-label">Name</label>
-                                <input type="text" className="form-control" name="name" value={input.name} onChange={inputHandler} />
+                                <input type="text" className="form-control" name="name" value={input.name} onChange={inputHandler} placeholder="Enter Name" />
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="form-label">Department</label>
-                                <input type="text" className="form-control" name="department" value={input.department} onChange={inputHandler} />
+                                <select className="form-select" name="department" value={input.department} onChange={inputHandler} >
+                                    <option value="">Select Department</option>
+                                    <option value="CS">CS</option>
+                                    <option value="CSE">CSE</option>
+                                    <option value="ECE">ECE</option>
+                                    <option value="EEE">EEE</option>
+                                    <option value="MECH">MECH</option>
+                                </select>
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="form-label">Semester</label>
-                                <input type="text" className="form-control" name="semester" value={input.semester} onChange={inputHandler} />
+                                <select className="form-select" name="semester" value={input.semester} onChange={inputHandler}>
+                                    <option value="">Select Semester</option>
+                                    <option value="1">S1</option>
+                                    <option value="2">S2</option>
+                                    <option value="3">S3</option>
+                                    <option value="4">S4</option>
+                                    <option value="5">S5</option>
+                                    <option value="6">S6</option>
+                                    <option value="7">S7</option>
+                                    <option value="8">S8</option>
+                                </select>
                             </div>                         
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="form-label">Course</label>
-                                <input type="text" className="form-control" name="course" value={input.course} onChange={inputHandler} />
+                                <select className="form-select" name="course" value={input.course} onChange={inputHandler}>
+                                    <option value="">Select Course</option>
+                                    <option value="MCA">MCA</option>
+                                    <option value="IMCA">IMCA</option>
+                                    <option value="B.Tech CS">B.Tech CS</option>
+                                    <option value="M.Tech CS">M.Tech CS</option>
+                                </select>
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="form-label">System Number</label>
